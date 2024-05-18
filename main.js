425b9617b61f81b8e5cdb1660f7eea593a0428cf
Alexander Gutierrez <alexzgut.dev@gmail.com>
2024-05-17 20:30:09 -0400

    run npm install, modify JSON response

--- main.js
+++ main.js
@@ -9,12 +9,37 @@ const app = http.createServer();
 
 app.on('request', (request, response) => {
     response.writeHead(httpStatus.StatusCodes.OK, {
-        'Content-type' : 'text/json'
+        'Content-type': 'text/json'
     });
 
-    let json = '{ "name" : "Alexander", "last_name" : "Gutierrez", "girlfriend" : "Tatiana Marquez", "city" : "Toronto", "country" : "Canada", "profession" : "Software Developer"}';
+    let json = 
+    `{
+        "name" : "Alexander",
+        "last_name" : "Gutierrez",
+        "girlfriend" : "Tatiana Marquez",
+        "city" : "Toronto",
+        "country" : "Canada",
+        "profession" : "Software Developer",
+        "Programming Languages" : [
+            "Java",
+            "Python",
+            "JavaScript",
+            "C#",
+        ],
+        "Programming Tools" : [
+            "VS Code",
+            "Git",
+            "Docker",
+            "Eclipse"
+        ]
+        "Frameworks" : [
+            "jQuery",
+            "Spring",
+            ".Net Core"
+        ]
+    }`;
 
     response.end(json);
 });
 
- app.listen(port);
+app.listen(port);
